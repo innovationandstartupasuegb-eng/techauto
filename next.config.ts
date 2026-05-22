79175f58d5b760a4e87805f8daacedc71a7dde2a
Anahit Arsen Serobyan <innovationandstartup.asuegb@gmail.com>
2026-05-22 13:20:04 +0400

    chore: redirect main page to login page

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   typescript: {
-    // Սա թույլ կտա Vercel-ին հաջողությամբ ավարտել build-ը՝ 
-    // անտեսելով TypeScript-ի տիպային սխալները
     ignoreBuildErrors: true,
+  },
+  // Ավելացնում ենք այս հրամանը՝ ավտոմատ լոգինի էջ տանելու համար
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/sign/sign/login',
+        permanent: true, // Սա ասում է բրաուզերին, որ վերահասցեավորումը մշտական է
+      },
+    ];
   },
 };
 
